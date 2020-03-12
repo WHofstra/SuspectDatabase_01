a7c13fad9e37778eafdf2de1895205dbf6351caa
Wayne Hofstra <wayne.hofstra@hva.nl>
2020-03-12 09:07:43 +0100

    Modified Courtcase-class

--- SuspectDatabase_01/Courtcase.h
+++ SuspectDatabase_01/Courtcase.h
@@ -15,20 +15,20 @@ class Courtcase
 		Courtcase();
 	    ~Courtcase();
 
-		void AddSuspect(Suspect aSuspect);
-		void AddAttorney(Attorney anAttorney);
-		void AddProsecutor(Prosecutor aProsecutor);
-		void AddJudge(Judge aJudge);
-		void AddOffence(Offence anOffence);
+		void AddSuspect(Suspect* aSuspect);
+		void AddAttorney(Attorney* anAttorney);
+		void AddProsecutor(Prosecutor* aProsecutor);
+		void AddJudge(Judge* aJudge);
+		void AddOffence(Offence* anOffence);
 
 		std::string GetSentence();
 
     private:
-		std::list<Suspect> suspectList;
-		std::list<Attorney> attorneyList;
-		std::list<Prosecutor> prosecutorList;
-		std::list<Judge> judgeList;
-		std::list<Offence> offenceList;
+		std::list<Suspect*> suspectList;
+		std::list<Attorney*> attorneyList;
+		std::list<Prosecutor*> prosecutorList;
+		std::list<Judge*> judgeList;
+		std::list<Offence*> offenceList;
 
 		std::string sentence;
 		struct tm startingDate;
